42bc8ca28639b2ece76fc25647c2ee97b150411d
Michał Kolany <kolany.michal@gmail.com>
2020-02-11 19:42:58 +0100

    v0.00 settings

--- main.py
+++ main.py
@@ -17,7 +17,19 @@ def zapisz(measured_temp, duty, set_temp):
 #### pid
 
 from PID import update
+Duty_min=0
+Duty_max=100
+set_temp = 30
 
+Temp_lista=[]
+Duty_lista=[]
+
+old_error = 0 
+old_time = 0
+measured_temp = 0
+p_term = 0
+i_term = 0 
+d_term = 0
 print("Duty range: [%d, %d]"%(Duty_min, Duty_max))
 
 print("PID constants: Kp=%d, Ki=%d, Kd=%d"%(kp,ki,kd))
