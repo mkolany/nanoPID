fe7ad868f628cc2b0aeb6a026cc5195271c769ce
Michał Kolany <kolany.michal@gmail.com>
2020-02-11 21:01:06 +0100

    v00 lipna

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ try:
                 old_time = now
                 measured_temp = sensor.initializer.value
                 
-                duty = sensor.initializer.update(old_time, old_error, measured_temp, set_temp)
+                duty = PID.update(old_time, old_error, measured_temp, set_temp)
                 heat_pwm.ChangeDutyCycle(duty)
 
                 Temp_lista.append(measured_temp)
